7d71587ea54180d1e4f97cfa87c8f742e6a5924e
Fabián Alexis Calderón Pérez <sabo0p731@gmail.com>
2024-04-06 23:46:58 -0300

    intento 1
    ya integre la firebase al proyecto, ahora solo necesito instalar unos schematics que me pide para instalar lo demás que me falta

---
+++ src/app/interface/interface.ts
@@ -0,0 +1,12 @@
+export interface Usuario{
+    ID_USER: string | undefined;
+    RUN_USER: number | undefined;
+    DIGITO_V_USER: string | undefined;
+    P_NOMBRE_USER: string | undefined;
+    S_NOMBRE_USER: string | undefined;
+    P_APELLIDO_USER: string | undefined;
+    M_APELLIDO_USER: string | undefined;
+    CORREO_ELECTRONICO_USER: string | undefined;
+    TELEFONO_USER: number | undefined;
+    FECHA_NACIMIENTO_USER: Date;
+}
